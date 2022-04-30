1b7e9d5cd5fa18edb28f5d37ab96c8c90a8564fe
Hansoleeya <yang11hs@naver.com>
2022-04-30 23:04:57 +0900

    Pseudo Selectors(가상 선택자)

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-as
+* as
 as을 사용하여 엘리먼트를 다른 엘리먼트로 교체할 수 있습니다.
 ex) Container as="header"
 https://styled-components.com/docs/basics#extending-styles
 
-Attaching additional props
+* Attaching additional props
 ex) const Input = styled.input.attrs({ required: true, minLength: 10 })``;
 https://styled-components.com/docs/basics#attaching-additional-props
--- src/App.js
+++ src/App.js
@@ -31,14 +31,30 @@ const Box = styled.div`
   height : 200px;
   width : 200px;
   background-color: tomato;
+  display: flex;
+  // justify-content 가로 축을 기준으로 좌우에 대한 정렬을 관장 합니다.
+  justify-content: center;
+  // align-items 세로 축을 기준으로 정렬을 하게 됩니다.
+  align-items: center;
   animation: ${animation} 1s linear infinite;
+  span{
+    font-size: 36px;
+    &:hover{
+      font-size: 46px;
+    }
+    &:active{
+      opacity:0;
+    }
+  }
 `;
 
 
 function App() {
   return (
     <Wrapper>
-      <Box/>
+      <Box>
+        <span>😍</span>
+        </Box>
     </Wrapper>
   );
 }
